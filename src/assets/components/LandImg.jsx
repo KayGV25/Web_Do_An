1d99ad94b2c3d474af2ec9b8efdd5a1c5b55eba2
KayGV25 <khuongvudang25@gmail.com>
2024-03-20 20:37:53 +0700

    add texting component frame

--- src/assets/components/LandImg.jsx
+++ src/assets/components/LandImg.jsx
@@ -2,7 +2,7 @@ function LandImg(){
     return(
         <>
             <div className="land-img">
-                <div className="left">
+                <div className="left-landing">
                     <div className="land-text">
                         Lorem ipsum dolor sit amet consectetur adipisicing elit. A consequuntur officia dignissimos nesciunt minima alias impedit eius optio, eum quia sit enim totam libero perferendis cumque fugit suscipit! Minus, commodi.
                     </div>
