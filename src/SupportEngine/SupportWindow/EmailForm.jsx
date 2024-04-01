8ecf8e0f3d9be99ea3a414e9118a954e8ef877de
Hikarius25 <quangminh25102005@gmail.com>
2024-04-01 13:23:10 +0700

    abc

--- src/SupportEngine/SupportWindow/EmailForm.jsx
+++ src/SupportEngine/SupportWindow/EmailForm.jsx
@@ -59,7 +59,7 @@ const EmailForm = props => {
 
     return (
         <div 
-        syles={{
+        style={{
             height: '100%',
             opacity: '1',
             height: props.visible ? '100%' : '0px',
