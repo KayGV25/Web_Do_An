24eb20e7daefeaa523b278689cbf2959639c83c3
KayGV25 <khuongvudang25@gmail.com>
2024-05-03 12:23:51 +0700

    finish admin page

--- src/FetchnUpdateData.jsx
+++ src/FetchnUpdateData.jsx
@@ -51,24 +51,30 @@ function FetchnUpdateData() {
 
     return (
         <div className="custom-background">
-            <div>
-            <h1 style={{ color: '#F0F0F0' , fontFamily: 'Display, sans-serif' }}>In stock</h1>
+            <div className="side-menu-container">
+                <div className="side-menu">
+                    <a href="#Instock">Instock</a>
+                    <a href="#Deli">Delivering</a>
+                </div>
+            </div>
+            <div style={{paddingTop: '30px'}}>
+            <h1 id="Instock" style={{ color: '#F0F0F0' , fontFamily: 'Display, sans-serif', fontSize: '5em', textAlign: 'center'}}>In stock</h1>
             {data.map((user, index) => {
                 if(user.status == 0){
                     return (
-                    <ul style={{ color: '#F0F0F0' }}>
-                        <div className="container">
-                            <div className="column instock">
+                    <ul style={{ color: '#F0F0F0', display: 'flex', justifyContent: 'center'}}>
+                        <div className="order-container">
+                            <div className="column">
                                 <li className="hidden">{user._id}</li>
-                                <li>Name: {user.name}</li>
+                                <li style={{fontSize: '2em'}}>Name: {user.name}</li>
                                 <li>Address: {user.address}</li>
                                 <li>Email: {user.email}</li>
                                 <li>Tel: {user.phone}</li>
                                 <li>Quantity: {user.productQuantity}</li>
                                 <li>Order time: {FormatDate(user.ordertime)}</li>
-                                <li>{user.status}</li>
+                                <li className="hidden">{user.status}</li>
                             </div>
-                            <div>
+                            <div className="btn-layout">
                                 <button onClick={() => handleDelivering(index,user._id)}>Delivering</button>
                                 <button onClick={() => handleDelivered(index,user._id)}>Delivered</button>
                                 <button onClick={() => handleDelete(index,user._id)}>Delete</button>
@@ -76,26 +82,26 @@ function FetchnUpdateData() {
                         </div>
                         </ul>    )}
         })}
-            </div> 
+            </div>
 
-            <div>
-            <h1 style={{ color: '#F0F0F0' , fontFamily: 'Display, sans-serif' }}>Delivering</h1>
+            <div style={{marginTop: '10vh'}}>
+            <h1 id="Deli" style={{color: '#F0F0F0' , fontFamily: 'Display, sans-serif', fontSize: '5em', textAlign: 'center'}}>Delivering</h1>
             {data.map((user, index) => {
                 if(user.status == 1){
                     return (
-                    <ul style={{ color: '#F0F0F0' }}>
-                        <div className="container">
-                            <div className="column delivering">
+                    <ul style={{ color: '#F0F0F0', display: 'flex', justifyContent: 'center' }}>
+                        <div className="order-container">
+                        <div className="column">
                                 <li className="hidden">{user._id}</li>
-                                <li>Name: {user.name}</li>
+                                <li style={{fontSize: '2em'}}>Name: {user.name}</li>
                                 <li>Address: {user.address}</li>
                                 <li>Email: {user.email}</li>
                                 <li>Tel: {user.phone}</li>
                                 <li>Quantity: {user.productQuantity}</li>
                                 <li>Order time: {FormatDate(user.ordertime)}</li>
-                                <li>{user.status}</li>
+                                <li className="hidden">{user.status}</li>
                             </div>
-                            <div>
+                            <div className="btn-layout">
                                 <button onClick={() => handleDelivering(index,user._id)}>Delivering</button>
                                 <button onClick={() => handleDelivered(index,user._id)}>Delivered</button>
                                 <button onClick={() => handleDelete(index,user._id)}>Delete</button>
