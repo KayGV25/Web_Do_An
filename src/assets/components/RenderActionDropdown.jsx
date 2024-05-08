c7e8a8ca6935c0810cbde75cb97c5a8b8fb406fb
KayGV25 <khuongvudang25@gmail.com>
2024-05-08 21:58:34 +0700

    Final AdminPage

---
+++ src/assets/components/RenderActionDropdown.jsx
@@ -0,0 +1,92 @@
+import { useState, useContext } from "react";
+import { AdminContext } from "../../pages/AdminPage";
+import axios from "axios";
+
+const LOOKUPSTATUS = {
+    0:"In Stock",
+    1:"Delivering",
+    2:"Delivered",
+    3:"Cancled",
+    "In stock": 0,
+    "Delivering": 1,
+    "Delivered": 2,
+    "Cancled": 3
+}
+
+function RenderActionDropdown(props){
+    const {UserData, setUserData} = useContext(AdminContext);
+    const [isClickable, setIsClickable] = useState(false);
+
+    function handleDelivering(index,id){
+        UserData[index].status = 1;
+        const newUserData = [...UserData];
+        setUserData(newUserData);
+        axios.post("https://be-inovation-project.onrender.com/updateStatus/OnTheWay", {
+            id: id,
+        })
+        .then(console.log("success"))
+        isClickable(false);
+    }
+
+    function handleDelivered(index,id){
+        UserData[index].status = 2;
+        const newUserData = [...UserData];
+        setUserData(newUserData);
+        axios.post("https://be-inovation-project.onrender.com/updateStatus/Delivered", {
+            id: id,
+        })
+        .then(console.log("success"))
+        isClickable(false);
+    }
+
+    function handleCancle(index,id){
+        UserData[index].status = 3;
+        const newUserData = [...UserData];
+        setUserData(newUserData);
+        axios.post("https://be-inovation-project.onrender.com/updateStatus/Cancled", {
+            id: id,
+        })
+        .then(console.log("success"))
+        isClickable(false);
+    }
+
+    function Update(){
+        setIsClickable(!isClickable);
+    }
+
+    function StateClass(status){
+        var res = "btn dropdown-toggle ";
+        if(status == 0){
+            res += "btn-danger";
+        }
+        else if(status == 1){
+            res += "btn-warning";
+        }
+        else if(status == 2){
+            res += "btn-success";
+        }
+        else{
+            res += "btn-secondary";
+        }
+        return res;
+    }
+
+    const user = props.data;
+    const index = props.index;
+    return(
+        <>
+            <div class="dropdown">
+                <button id={user._id} className={StateClass(user.status)} type="button" data-bs-toggle="dropdown" aria-expanded="false" disabled={(!isClickable) ? true : false}>
+                    {LOOKUPSTATUS[user.status]}
+                </button>
+                <ul class="dropdown-menu">
+                    {(user.status != 1) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivering(index, user._id)}}>Delivering</a></li> : <></>}
+                    {(user.status != 2 && user.status != 0) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivered(index, user._id)}}>Delivered</a></li> : <></>}
+                    <li><a class="dropdown-item" href="#" onClick={()=>{handleCancle(index, user._id)}}>Cancle</a></li>
+                </ul>
+            </div>
+            <button className="update-btn" onClick={()=>{Update(user._id)}}>Update</button>
+        </>
+    )
+}
+export default RenderActionDropdown;
--- src/assets/components/RenderData.jsx
+++ src/assets/components/RenderData.jsx
@@ -1,86 +1,16 @@
 import "bootstrap/dist/css/bootstrap.min.css";
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { AdminContext } from "../../pages/AdminPage";
-import axios from "axios";
 import RenderName from "./RenderName";
-import { Button } from "react-bootstrap";
-const LOOKUPSTATUS = {
-    0:"In Stock",
-    1:"Delivering",
-    2:"Delivered",
-    3:"Cancled",
-    "In stock": 0,
-    "Delivering": 1,
-    "Delivered": 2,
-    "Cancled": 3
-}
+import RenderActionDropdown from "./RenderActionDropdown";
+
+
 function RenderData(){
     const {UserData, setUserData} = useContext(AdminContext);
 
     function FormatDate(rawDate){
         var d = new Date(rawDate);
         return d.toLocaleString('vi-VN');
-    }
-
-    function handleDelivering(index,id){
-        UserData[index].status = 1;
-        const newUserData = [...UserData];
-        setUserData(newUserData);
-        axios.post("https://be-inovation-project.onrender.com/updateStatus/OnTheWay", {
-            id: id,
-        })
-        .then(console.log("success"))
-    }
-
-    function handleDelivered(index,id){
-        UserData[index].status = 2;
-        const newUserData = [...UserData];
-        setUserData(newUserData);
-        axios.post("https://be-inovation-project.onrender.com/updateStatus/Delivered", {
-            id: id,
-        })
-        .then(console.log("success"))
-    }
-
-    function handleCancle(index,id){
-        UserData[index].status = 3;
-        const newUserData = [...UserData];
-        setUserData(newUserData);
-        axios.post("https://be-inovation-project.onrender.com/updateStatus/Cancled", {
-            id: id,
-        })
-        .then(console.log("success"))
-    }
-
-    function Update(index, id){
-        const status = document.getElementById(id).innerText;
-        const GetStatus = LOOKUPSTATUS[status];
-        if(GetStatus == 1){
-            handleDelivering(index, id);
-        }
-        else if(GetStatus == 2){
-            handleDelivered(index, id);
-        }
-        else if(GetStatus == 3){
-            handleCancle(index, id);
-        }
-    }
-
-    function StateClass(status){
-        var res = "btn dropdown-toggle ";
-        if(status == 0){
-            res += "btn-danger";
-        }
-        else if(status == 1){
-            res += "btn-warning";
-        }
-        else if(status == 2){
-            res += "btn-success";
-        }
-        else{
-            res += "btn-secondary";
-        }
-        return res;
     }
 
     // Mapping prioritize instock and delivering
@@ -93,17 +23,7 @@ function RenderData(){
                                 <p>{user._id}</p>
                                 <p>{FormatDate(user.ordertime)}</p>
                                 <RenderName data={user}/>
-                                <div class="dropdown">
-                                    <button id={user._id} className={StateClass(user.status)} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        {LOOKUPSTATUS[user.status]}
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        {(user.status != 1) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivering(index, user._id)}}>Delivering</a></li> : <></>}
-                                        {(user.status != 2 && user.status != 0) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivered(index, user._id)}}>Delivered</a></li> : <></>}
-                                        <li><a class="dropdown-item" href="#" onClick={()=>{handleCancle(index, user._id)}}>Cancle</a></li>
-                                    </ul>
-                                </div>
-                                <button className="update-btn" onClick={()=>{Update(index, user._id)}}>Update</button>
+                                <RenderActionDropdown data={user} index={index}/>
                             </div>
                         )
                     }
@@ -115,17 +35,7 @@ function RenderData(){
                                 <p>{user._id}</p>
                                 <p>{FormatDate(user.ordertime)}</p>
                                 <RenderName data={user}/>
-                                <div class="dropdown">
-                                    <button id={user._id} className={StateClass(user.status)} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        {LOOKUPSTATUS[user.status]}
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        {(user.status != 1) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivering(index, user._id)}}>Delivering</a></li> : <></>}
-                                        {(user.status != 2 && user.status != 0) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivered(index, user._id)}}>Delivered</a></li> : <></>}
-                                        <li><a class="dropdown-item" href="#" onClick={()=>{handleCancle(index, user._id)}}>Cancle</a></li>
-                                    </ul>
-                                </div>
-                                <button className="update-btn" onClick={()=>{Update(index, user._id)}}>Update</button>
+                                <RenderActionDropdown data={user} index={index}/>
                             </div>
                         )
                     }
@@ -137,17 +47,7 @@ function RenderData(){
                                 <p>{user._id}</p>
                                 <p>{FormatDate(user.ordertime)}</p>
                                 <RenderName data={user}/>
-                                <div class="dropdown">
-                                    <button id={user._id} className={StateClass(user.status)} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        {LOOKUPSTATUS[user.status]}
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        {(user.status != 1) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivering(index, user._id)}}>Delivering</a></li> : <></>}
-                                        {(user.status != 2 && user.status != 0) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivered(index, user._id)}}>Delivered</a></li> : <></>}
-                                        <li><a class="dropdown-item" href="#" onClick={()=>{handleCancle(index, user._id)}}>Cancle</a></li>
-                                    </ul>
-                                </div>
-                                <button className="update-btn" onClick={()=>{Update(index, user._id)}}>Update</button>
+                                <RenderActionDropdown data={user} index={index}/>
                             </div>
                         )
                     }
@@ -159,17 +59,7 @@ function RenderData(){
                                 <p>{user._id}</p>
                                 <p>{FormatDate(user.ordertime)}</p>
                                 <RenderName data={user}/>
-                                <div class="dropdown">
-                                    <button id={user._id} className={StateClass(user.status)} type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                        {LOOKUPSTATUS[user.status]}
-                                    </button>
-                                    <ul class="dropdown-menu">
-                                        {(user.status != 1) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivering(index, user._id)}}>Delivering</a></li> : <></>}
-                                        {(user.status != 2 && user.status != 0) ? <li><a class="dropdown-item" href="#" onClick={() => {handleDelivered(index, user._id)}}>Delivered</a></li> : <></>}
-                                        <li><a class="dropdown-item" href="#" onClick={()=>{handleCancle(index, user._id)}}>Cancle</a></li>
-                                    </ul>
-                                </div>
-                                <button className="update-btn" onClick={()=>{Update(index, user._id)}}>Update</button>
+                                <RenderActionDropdown data={user} index={index}/>
                             </div>
                         )
                     }
